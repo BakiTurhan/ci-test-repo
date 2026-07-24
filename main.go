aec1f6e78dbc140ecf4ca8ebb0ad1fd1b28297a7
Baki Turhan <bakiturhansm@gmail.com>
2026-07-24 16:30:53 +0300

    ,

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	Email string `validate:"required,email"`
+}
 
 func main() {
-	fmt.Println("CI Test")
+	// Validator'ı kullanalım
+	validate := validator.New()
+	user := User{Email: "test@test.com"}
+	err := validate.Struct(user)
+	if err != nil {
+		fmt.Println("Validation error:", err)
+	}
+
+	// Crypto'yu kullanalım
+	password := []byte("gizlisifre")
+	hashedPassword, _ := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+	fmt.Println("Hashed password:", string(hashedPassword))
 }
